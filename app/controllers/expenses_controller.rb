cf6e0d9abc45fbe502d24c0e54df6aea25704614
Damian Malysza <damian.malysza@gmail.com>
2022-02-08 21:04:09 -0500

    added controller action to handle creation of expenses for a budget

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,27 @@
 class ExpensesController < ApplicationController
 
   def create
-
+    budget =  Budget.find(params[:budget_id])
+    if budget
+      budget.expenses.build(expense_params)
+      if budget.save
+        render json: BudgetSerializer.new(budget), status: :created
+      else
+        render json: {errors: budget.errors.full_messages}, status: :unprocessable_entity
+      end
+    else
+      render status: :unprocessable_entity
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount)
   end
 
 end
